c17150c88d43dcd2f371970e5ff4117cbb83d050
Oren <orengolan@gmail.com>
2017-07-04 22:10:53 -0700

    hardcoding two awesome ponies

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -6,11 +6,11 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>Preact App</h1>
+				<h1>My Little Pony</h1>
 				<nav>
 					<Link activeClassName={style.active} href="/">Home</Link>
-					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
+					<Link activeClassName={style.active} href="/profile">Checkout</Link>
+					<Link activeClassName={style.active} href="/profile/john">Logout</Link>
 				</nav>
 			</header>
 		);
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,15 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
+				<h1>Ponies</h1>
+          <div class={style.list}>
+            <div>Rarity</div>
+            <img src="https://mylittlepony.hasbro.com/images/pick_EG_fluttershy.png"></img>
+          </div>
+          <div class={style.list}>
+            <div>Rainbow Dash</div>
+            <img src="http://vignette3.wikia.nocookie.net/mlp/images/1/11/FANMADE_Rainbow_Dash_flying_2.png/revision/latest?cb=20131218193139"></img>
+          </div>
 			</div>
 		);
 	}
